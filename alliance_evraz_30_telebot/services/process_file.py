dbb7ae03784f51175b929bf96ec2ce6856c2ccf6
Vladislav Yakonyuk <vladislavvogonov@gmail.com>
2024-11-30 23:36:26 +0300

    Enhance bot responses in bot.py and process_file.py for improved user experience. Updated messages to include better formatting and clearer instructions regarding file submissions and code reviews. Emphasized the bot's purpose and capabilities with more context in responses.

--- alliance_evraz_30_telebot/services/process_file.py
+++ alliance_evraz_30_telebot/services/process_file.py
@@ -8,8 +8,8 @@ from entryponts.entrypoint import runLLM
 def process_zip_file(bot: TeleBot, file_id: str, file_name: str, chat_id: int) -> str:
     local_file_path = save_file(bot, file_id, generate_unique_file_name(chat_id, file_name))
 
-    bot.send_message(chat_id, "Я отправил ваш проект на code-review." +
-                     "Это займет несколько минут. Пожалуйста, не беспокойте меня в это время." + 
+    bot.send_message(chat_id, "Я отправил ваш проект на code-review. " +
+                     "Это займет несколько минут. Пожалуйста, не беспокойте меня в это время. " + 
                      "Когда работа завершится, я отправлю вам результат в чат."
     )
 
