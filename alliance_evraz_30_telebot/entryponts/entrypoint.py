2c1f6a30633eb564fe3e3ede69fa721c2b43ad04
IYuFedotov <40522008+barmatey@users.noreply.github.com>
2024-12-01 01:09:10 +0300

    IVAN

--- alliance_evraz_30_telebot/entryponts/entrypoint.py
+++ alliance_evraz_30_telebot/entryponts/entrypoint.py
@@ -1,34 +1,43 @@
 import json
 from pathlib import Path
 
-from adapters.llm import LLMAdapter
-from constants.prompts import get_prompts, get_summarise_prompts
-from services.services import convert_path_to_structure, split_text_in_chunks
-
+from adapters import llm
+from constants import prompts
+from services import services
 
 def runLLM(project_path: Path, excludes: set[str]) -> str:
-    adapter = LLMAdapter()
+    adapter = llm.LLMAdapter()
 
-    structure = convert_path_to_structure(project_path, excludes)
+    structure = services.convert_path_to_structure(project_path, excludes)
     structure = json.dumps(structure)
 
-    chunks = split_text_in_chunks(structure)
+    chunks = services.split_text_in_chunks(structure)
 
     contents: list[str] = []
     for chunk in chunks:
-        prompts_to_send = get_prompts()
-        prompts_to_send.append(chunk)
+        prompts_to_send = [
+            prompts.about_architecture(),
+            prompts.find_problems(),
+            chunk,
+            prompts.response_format(len(chunk) // 2),
+        ]
         response = adapter.send_prompts(prompts_to_send)
         contents.append(response.get_content())
 
     if len(contents) > 1:
-        prompts_to_send = get_summarise_prompts()
-        prompts_to_send.extend(contents)
+        prompts_to_send = [
+            prompts.summarise(),
+            *contents
+        ]
         response = adapter.send_prompts(prompts_to_send)
         content = response.get_content()
     else:
         content = contents[0]
 
-    response = adapter.send_prompts(["Translate this text in russian: " + content])
+    response = adapter.send_prompts([
+        prompts.translate(),
+        content,
+    ])
+    content = response.get_content()
+    return content
 
-    return response.get_content()
